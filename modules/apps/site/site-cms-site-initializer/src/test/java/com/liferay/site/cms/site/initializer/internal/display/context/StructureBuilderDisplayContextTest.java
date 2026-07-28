b34924dfc49e95eadd022972975b37c2c5ef4039
smottal <117469730+smottal@users.noreply.github.com>
2026-07-28 17:46:06 +0200

    LPD-99448 Update CMS Unit tests

---
+++ modules/apps/site/site-cms-site-initializer/src/test/java/com/liferay/site/cms/site/initializer/internal/display/context/StructureBuilderDisplayContextTest.java
@@ -0,0 +1,131 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.display.context;
+
+import com.liferay.object.admin.rest.dto.v1_0.ObjectDefinition;
+import com.liferay.object.admin.rest.resource.v1_0.ObjectDefinitionResource;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.site.cms.site.initializer.contributor.CMSStructureObjectFolderContributor;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Stefano Motta
+ */
+public class StructureBuilderDisplayContextTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testGetBaseObjectDefinition() throws Exception {
+		String baseObjectDefinitionExternalReferenceCode =
+			StringUtil.randomString();
+
+		ObjectDefinition baseObjectDefinition = new ObjectDefinition();
+
+		ObjectDefinitionResource objectDefinitionResource = Mockito.mock(
+			ObjectDefinitionResource.class);
+
+		Mockito.when(
+			objectDefinitionResource.getObjectDefinitionByExternalReferenceCode(
+				baseObjectDefinitionExternalReferenceCode)
+		).thenReturn(
+			baseObjectDefinition
+		);
+
+		ObjectDefinitionResource.Builder builder = Mockito.mock(
+			ObjectDefinitionResource.Builder.class);
+
+		Mockito.when(
+			builder.user(Mockito.any(User.class))
+		).thenReturn(
+			builder
+		);
+
+		Mockito.when(
+			builder.build()
+		).thenReturn(
+			objectDefinitionResource
+		);
+
+		ObjectDefinitionResource.Factory factory = Mockito.mock(
+			ObjectDefinitionResource.Factory.class);
+
+		Mockito.when(
+			factory.create()
+		).thenReturn(
+			builder
+		);
+
+		CMSStructureObjectFolderContributor
+			cmsStructureObjectFolderContributor = Mockito.mock(
+				CMSStructureObjectFolderContributor.class);
+
+		String objectFolderExternalReferenceCode = StringUtil.randomString();
+
+		Mockito.when(
+			cmsStructureObjectFolderContributor.
+				getObjectFolderExternalReferenceCode()
+		).thenReturn(
+			objectFolderExternalReferenceCode
+		);
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY,
+			new ThemeDisplay() {
+				{
+					setUser(Mockito.mock(User.class));
+				}
+			});
+		mockHttpServletRequest.setParameter(
+			"objectFolderExternalReferenceCode",
+			objectFolderExternalReferenceCode);
+
+		StructureBuilderDisplayContext structureBuilderDisplayContext =
+			new StructureBuilderDisplayContext(
+				List.of(cmsStructureObjectFolderContributor),
+				mockHttpServletRequest, null, factory, null);
+
+		Assert.assertNull(
+			ReflectionTestUtil.invoke(
+				structureBuilderDisplayContext, "_getBaseObjectDefinition",
+				new Class<?>[0]));
+
+		Mockito.when(
+			cmsStructureObjectFolderContributor.
+				getBaseObjectDefinitionExternalReferenceCode()
+		).thenReturn(
+			baseObjectDefinitionExternalReferenceCode
+		);
+
+		Assert.assertSame(
+			baseObjectDefinition,
+			ReflectionTestUtil.invoke(
+				structureBuilderDisplayContext, "_getBaseObjectDefinition",
+				new Class<?>[0]));
+	}
+
+}
