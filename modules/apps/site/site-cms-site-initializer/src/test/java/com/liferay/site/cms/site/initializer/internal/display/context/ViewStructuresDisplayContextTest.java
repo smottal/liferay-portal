98a0f0f5d697d97e6e6debe1c46126311324deb0
smottal <117469730+smottal@users.noreply.github.com>
2026-07-28 17:46:06 +0200

    LPD-XXX Update CMS Unit tests

--- modules/apps/site/site-cms-site-initializer/src/test/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewStructuresDisplayContextTest.java
+++ modules/apps/site/site-cms-site-initializer/src/test/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewStructuresDisplayContextTest.java
@@ -5,15 +5,26 @@
 
 package com.liferay.site.cms.site.initializer.internal.display.context;
 
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.site.cms.site.initializer.contributor.CMSStructureObjectFolderContributor;
 
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import org.mockito.Mockito;
 
 import org.springframework.mock.web.MockHttpServletRequest;
 
@@ -37,6 +48,79 @@ public class ViewStructuresDisplayContextTest {
 
 		Assert.assertTrue(filterString.startsWith(StringPool.OPEN_PARENTHESIS));
 		Assert.assertTrue(filterString.endsWith(StringPool.CLOSE_PARENTHESIS));
+	}
+
+	@Test
+	public void testGetCreationMenu() {
+		_setUpLanguageUtil();
+
+		ViewStructuresDisplayContext viewStructuresDisplayContext1 =
+			_getViewStructuresDisplayContext(null);
+
+		CreationMenu creationMenu1 =
+			viewStructuresDisplayContext1.getCreationMenu();
+
+		List<DropdownItem> dropdownItems1 = _getPrimaryItems(creationMenu1);
+
+		int size = dropdownItems1.size();
+
+		ViewStructuresDisplayContext viewStructuresDisplayContext2 =
+			_getViewStructuresDisplayContext(StringUtil.randomString());
+
+		CreationMenu creationMenu2 =
+			viewStructuresDisplayContext2.getCreationMenu();
+
+		List<DropdownItem> dropdownItems2 = _getPrimaryItems(creationMenu2);
+
+		Assert.assertEquals(
+			dropdownItems2.toString(), size + 1, dropdownItems2.size());
+	}
+
+	private List<DropdownItem> _getPrimaryItems(CreationMenu creationMenu) {
+		return (List<DropdownItem>)creationMenu.get("primaryItems");
+	}
+
+	private ViewStructuresDisplayContext _getViewStructuresDisplayContext(
+		String objectFolderExternalReferenceCode) {
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY,
+			new ThemeDisplay() {
+				{
+					setPathFriendlyURLPublic(StringUtil.randomString());
+				}
+			});
+
+		CMSStructureObjectFolderContributor
+			cmsStructureObjectFolderContributor = Mockito.mock(
+				CMSStructureObjectFolderContributor.class);
+
+		Mockito.when(
+			cmsStructureObjectFolderContributor.
+				getObjectFolderExternalReferenceCode()
+		).thenReturn(
+			objectFolderExternalReferenceCode
+		);
+
+		Mockito.lenient(
+		).when(
+			cmsStructureObjectFolderContributor.getLabel()
+		).thenReturn(
+			StringUtil.randomString()
+		);
+
+		return new ViewStructuresDisplayContext(
+			List.of(cmsStructureObjectFolderContributor),
+			mockHttpServletRequest);
+	}
+
+	private void _setUpLanguageUtil() {
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(Mockito.mock(Language.class));
 	}
 
 	private final ViewStructuresDisplayContext _viewStructuresDisplayContext =
---
+++ modules/apps/site/site-cms-site-initializer/src/test/java/com/liferay/site/cms/site/initializer/internal/struts/UpdateStructureStrutsActionTest.java
@@ -0,0 +1,120 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.struts;
+
+import com.liferay.object.admin.rest.dto.v1_0.ObjectDefinition;
+import com.liferay.object.admin.rest.dto.v1_0.ObjectDefinitionSetting;
+import com.liferay.object.admin.rest.resource.v1_0.ObjectDefinitionResource;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Stefano Motta
+ */
+public class UpdateStructureStrutsActionTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testUpdateObjectDefinition() throws Exception {
+		ObjectDefinition existingObjectDefinition = new ObjectDefinition();
+		String key1 = RandomTestUtil.randomString();
+
+		existingObjectDefinition.setObjectDefinitionSettings(
+			new ObjectDefinitionSetting[] {
+				_createObjectDefinitionSetting("acceptAllGroups", "true"),
+				_createObjectDefinitionSetting(
+					"allowStandaloneObjectEntry", "true"),
+				_createObjectDefinitionSetting(
+					key1, RandomTestUtil.randomString())
+			});
+
+		String titleObjectFieldName = RandomTestUtil.randomString();
+
+		existingObjectDefinition.setTitleObjectFieldName(titleObjectFieldName);
+
+		long objectDefinitionId = RandomTestUtil.randomLong();
+
+		ObjectDefinitionResource objectDefinitionResource = Mockito.mock(
+			ObjectDefinitionResource.class);
+
+		Mockito.when(
+			objectDefinitionResource.getObjectDefinition(objectDefinitionId)
+		).thenReturn(
+			existingObjectDefinition
+		);
+
+		ObjectDefinition objectDefinition = new ObjectDefinition();
+
+		String key2 = RandomTestUtil.randomString();
+		String value1 = RandomTestUtil.randomString();
+		String value2 = RandomTestUtil.randomString();
+
+		objectDefinition.setObjectDefinitionSettings(
+			new ObjectDefinitionSetting[] {
+				_createObjectDefinitionSetting(key1, value1),
+				_createObjectDefinitionSetting(key2, value2)
+			});
+
+		ReflectionTestUtil.invoke(
+			new UpdateStructureStrutsAction(), "_updateObjectDefinition",
+			new Class<?>[] {
+				ObjectDefinition.class, long.class,
+				ObjectDefinitionResource.class
+			},
+			objectDefinition, objectDefinitionId, objectDefinitionResource);
+
+		ObjectDefinitionSetting[] objectDefinitionSettings =
+			objectDefinition.getObjectDefinitionSettings();
+
+		Map<String, Object> valuesMap = new HashMap<>();
+
+		for (ObjectDefinitionSetting objectDefinitionSetting :
+				objectDefinitionSettings) {
+
+			valuesMap.put(
+				objectDefinitionSetting.getName(),
+				objectDefinitionSetting.getValue());
+		}
+
+		Assert.assertFalse(valuesMap.containsKey("acceptAllGroups"));
+		Assert.assertFalse(
+			valuesMap.containsKey("acceptedGroupExternalReferenceCodes"));
+		Assert.assertFalse(valuesMap.containsKey("allowStandaloneObjectEntry"));
+		Assert.assertEquals(value1, valuesMap.get(key1));
+		Assert.assertEquals(value2, valuesMap.get(key2));
+
+		Assert.assertEquals(
+			titleObjectFieldName, objectDefinition.getTitleObjectFieldName());
+	}
+
+	private ObjectDefinitionSetting _createObjectDefinitionSetting(
+		String name, Object value) {
+
+		ObjectDefinitionSetting objectDefinitionSetting =
+			new ObjectDefinitionSetting();
+
+		objectDefinitionSetting.setName(name);
+		objectDefinitionSetting.setValue(value);
+
+		return objectDefinitionSetting;
+	}
+
+}
