d192ca7cdb1d509e451038146b1bd0b5c3df6f1a
smottal <117469730+smottal@users.noreply.github.com>
2026-08-03 11:03:36 +0200

    LPD-99448 Apply frontend changes

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorToolbar.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorToolbar.tsx
@@ -250,32 +250,6 @@ export default function ContentEditorToolbar({
 			);
 	}, [getForm, localizationLanguageId]);
 
-	useEffect(() => {
-		const preventButtonSubmit = (event: MouseEvent) => {
-			const target = event.target as HTMLElement | null;
-
-			const button = target?.closest?.(
-				'.cms-object-layout-form .component-tabs button'
-			) as HTMLButtonElement | null;
-
-			if (!button) {
-				return;
-			}
-
-			const type = button.getAttribute('type');
-
-			if (!type || type === 'submit') {
-				event.preventDefault();
-			}
-		};
-
-		document.addEventListener('click', preventButtonSubmit, true);
-
-		return () => {
-			document.removeEventListener('click', preventButtonSubmit, true);
-		};
-	}, []);
-
 	return (
 		<Toolbar
 			backURL={backURL}
